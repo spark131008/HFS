20b6bf859939176ab6119ded79eaa6afa888bc73
Nishanth Prabhu <nishbprabhu@gmail.com>
2025-04-01 21:01:25 -0700

    Fixed components path

--- app/landing/components/Header.tsx
+++ app/landing/components/Header.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Button } from "../../../components/ui/button";
 
 type User = {
   name: string;
